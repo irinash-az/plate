50d9d432ef0492e2ba01f28e891b9edb32fd4709
Irina Shchedrina <irinash@amazon.com>
2021-08-06 16:52:51 -0700

    Replace list block type names

--- packages/elements/list/src/defaults.ts
+++ packages/elements/list/src/defaults.ts
@@ -1,6 +1,6 @@
-export const ELEMENT_UL = 'ul';
-export const ELEMENT_OL = 'ol';
-export const ELEMENT_LI = 'li';
-export const ELEMENT_LIC = 'lic';
+export const ELEMENT_UL = 'unordered-list';
+export const ELEMENT_OL = 'ordered-list';
+export const ELEMENT_LI = 'list-item';
+export const ELEMENT_LIC = 'list-item-child';
 
 export const KEYS_LIST = [ELEMENT_UL, ELEMENT_OL, ELEMENT_LI, ELEMENT_LIC];
